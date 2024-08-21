4393714d6c6a5086386ca615f1bc757674f844af
shotfirescout <murilonata376@gmail.com>
2024-08-21 19:22:44 -0300

    Add files via upload

---
+++ calculadoraSimplesJava/src/main/java/com/uniensinojavaswing/murilo/calculadorasimplesjava/CalculadoraSimplesJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uniensinojavaswing.murilo.calculadorasimplesjava;
+
+/**
+ *
+ * @author muril
+ */
+public class CalculadoraSimplesJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
